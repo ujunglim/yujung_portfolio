092473313f7e1c7b3c36f43883c288229b36487b
yujung <ujunglim@naver.com>
2024-03-29 17:20:12 +0900

    [feat] 다크모드 추가, useThemeSwitcher훅

--- components/layout/AppHeader.tsx
+++ components/layout/AppHeader.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { FiX, FiMenu } from "react-icons/fi";
+import { FiMoon, FiSun, FiX, FiMenu } from "react-icons/fi";
+import useThemeSwitcher from "../../hooks/useThemeSwitcher";
 
 function AppHeader() {
   const [showMenu, setShowMenu] = useState(false);
+  const [activeTheme, setTheme] = useThemeSwitcher();
 
   function toggleMenu() {
     if (!showMenu) {
@@ -24,11 +26,12 @@ function AppHeader() {
       {/* 헤더 */}
       <div className="z-10 max-w-screen-lg xl:max-w-screen-xl block sm:flex sm:justify-between sm:items-center py-6">
         <div className="flex justify-between items-center px-4 sm:px-0">
-          <div>
+          {/* 로고 */}
+          <div className="dark:text-ternary-light">
             <Link href="/">YuJung</Link>
           </div>
 
-          {/* 메뉴 버튼 */}
+          {/* 작은 화면의 햄버거메뉴 */}
           <div className="sm:hidden">
             <button
               onClick={toggleMenu}
@@ -51,7 +54,7 @@ function AppHeader() {
           </div>
         </div>
 
-        {/* 작은 화면에서 헤더 */}
+        {/* 작은 화면의 메뉴 */}
         <div
           className={
             showMenu
@@ -76,7 +79,7 @@ function AppHeader() {
           </div>
         </div>
 
-        {/* 큰 화면에서 헤더 */}
+        {/* 큰 화면에서 메뉴 */}
         <div className="font-general-medium hidden m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
           <div
             className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
@@ -102,10 +105,15 @@ function AppHeader() {
         {/* 헤더 우측 theme switcher*/}
         <div className="hidden sm:flex justify-between items-center flex-col md:flex-row">
           <div
+            onClick={() => setTheme(activeTheme)}
             aria-label="Theme Switcher"
             className="ml-8 bg-primary-light dark:bg-ternary-dark p-3 shadow-sm rounded-xl cursor-pointer"
           >
-            swither
+            {activeTheme === "dark" ? (
+              <FiMoon className="text-ternary-dark hover:text-gray-400 dark:text-ternary-light dark:hover:text-primary-light text-xl" />
+            ) : (
+              <FiSun className="text-gray-200 hover:text-gray-50 text-xl" />
+            )}
           </div>
         </div>
       </div>
